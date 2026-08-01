5b3429943ed747bf791ae62d644dab2f842d8ac6
DAN-BULL1210 <giulietta33@o3i5o.com>
2026-08-01 16:42:41 +0900

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,63 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function Home() {
+  //[記憶の準備]
+  //現在の値の箱と、スイッチを作る
+  const [ status, setStatus] = useState(1);
+  const [current, setCurrent] = useState("");
+  
+  useEffect(() => {
+    const updateTime =() => {
+      const now = new Date();
+      setCurrent(now.toLocaleTimeString("ja-JP", { hour: "2-digit", minute: "2-digit" }));
+     };
+
+     updateTime();//1回だけ実行される これを書かないと1秒間時間が表示されない
+
+     //1秒ごとにupdateTimeを繰り返すタイマー
+     const timerId = setInterval(updateTime, 1000);
+
+     //画面を閉じるときにタイマー解除　アンマウント
+     return () => clearInterval(timerId); //return()クリーンアップ関数
+  },[]);
+
+  const handleDrink = async () => {
+    const { error } = await supabase
+    .from('medication_logs')
+    .insert([
+      {
+        user_id: '123e4567-e89b-12d3-a456-426614174000', 
+          target_date: '2026-07-31', 
+          time_slot: 'noon'
+      }
+    ]);
+
+    if (error) {
+      console.error("保存エラー:", error);
+    } else {
+       setStatus(status + 1);
+    }
+  };
+  //[見た目]
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <main className="p-20 text-center">
+      <h1 className="text-4xl font-bold mb-8">
+         {status === 1 ? "お薬飲んだ？" : "Good Job"}
+      </h1>
+      <div className="text-6xl font-mono mb-10 text-gray-700 font-bold tracking-widest">
+        {current || "--:--"}
+      </div>
+
+      <button 
+        onClick={handleDrink}
+        disabled = {status === 2}
+        className="bg-blue-500 text-white font-bold py-4 px-8 rounded-xl"
+        >
+          {status === 1 ? "飲んだ" : "完了"}
+        </button>
+    </main>
+  )
 }
